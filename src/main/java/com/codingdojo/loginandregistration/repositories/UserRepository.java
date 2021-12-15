f368d47a62285c12052b29556a53fc2d26b8746b
peter boulton <peterboulotn1228@gmail.com>
2021-12-14 18:09:23 -0800

    first commit

---
+++ src/main/java/com/codingdojo/loginandregistration/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.codingdojo.loginandregistration.repositories;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.loginandregistration.model.User;
+
+    
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    Optional<User> findByEmail(String email);
+    
+}
